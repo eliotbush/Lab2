f9dc3ed03af3989d8adcad8ed045ab0a4fc85ca2
Ygorsunny Jean <ygorsunnyjean@1x-vl921-72-19-112-22.wireless.umass.edu>
2015-10-25 23:38:18 -0400

    Haven

--- testImmediate.c
+++ testImmediate.c
@@ -30,13 +30,30 @@ bool verifyImmediate(char *imm){
 */
 
 int main(int argc, const char * argv[]) {
-    printf("%s\n",argv[1]);
-    char imm = *(argv[1]);
+    int i;
+    int j;
+    const char * strChar;
+    printf("%d\n",argc);
+    for(i=1;i<argc;i++){
+        printf("%s\n",argv[i]);
+        strChar = argv[i];
+        for(j = 0; j<strlen(strChar); j++){
+            printf("%c\n",strChar[0]);//this line is a char in the string array!
+        }
+        
+    }
+
+    
+    
+    
+    
+    /*char imm = *(argv[1]);
     if(imm=='g'){printf("Word\n");}
     else(printf("Still grinding\n"));
     if((imm -'0')<0 || (imm -'0'>9)){printf("Not valid\n");}
     else(printf("Valid\n"));
     return 0;
+     */
 }
 
 
