683eb799b2f7c830efc499d981daaabcdd27764d
goon034 <ygorsunnyjean@gmail.com>
2015-10-27 18:11:44 -0400

    test for verifyImmediate

--- testImmediate.c
+++ testImmediate.c
@@ -1,38 +1,18 @@
-//
-//  testImmediate.c
-//  
-//
-//  Created by Ygorsunny Jean on 10/25/15.
-//
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
 
-/*
-bool verifyImmediate(char *);
 
-bool verifyImmediate(char *imm){
-    
-    if((imm[0]-'0')<0 || (imm[0]-'0')>9){printf("not immediate: %s", imm); return false;}
-    
-    
-    int i;
-    //while pointer is pointing to a valid integer..
-    while(imm[i]-'0'>=0){
-        char c = *imm;
-        int num = c-'0';
-        i++;
-    }
-    
-
-    
-    return true;}
-*/
 
 int main(int argc, const char * argv[]) {
     int i;
     int j;
+    int value;
     const char * strChar;
+    const char * neg;
     printf("%d\n",argc);
     for(i=1;i<argc;i++){
         printf("%s\n",argv[i]);
@@ -42,19 +22,6 @@ int main(int argc, const char * argv[]) {
         }
         
     }
-
-    
-    
-    
-    
-    /*char imm = *(argv[1]);
-    if(imm=='g'){printf("Word\n");}
-    else(printf("Still grinding\n"));
-    if((imm -'0')<0 || (imm -'0'>9)){printf("Not valid\n");}
-    else(printf("Valid\n"));
-    */
     return 0;
-
-}
-
+    }
 
